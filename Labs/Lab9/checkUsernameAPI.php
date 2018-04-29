fb198fec2b9702cb9cea7d4e7afdc4f422151768
Andrew Marmolejo <amarmolejo@csumb.edu>
2018-04-29 03:01:01 +0000

    complete

---
+++ Labs/Lab9/checkUsernameAPI.php
@@ -0,0 +1,22 @@
+<?php
+
+    include '../../dbConnection.php';
+    
+    $conn = getDatabaseConnection("heroku_43c1456b693fb28");
+    $username = $_GET['username'];
+    
+    //next query allows for SQL injection because of the single quotes
+    //$sql = "SELECT * FROM lab9_user WHERE username = '$username'";
+    
+    $sql = "SELECT * FROM lab9_user WHERE username = :username";
+    
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->execute( array(":username"=>$username));
+    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    //print_r($record);
+    
+    echo json_encode($record);
+
+?>
